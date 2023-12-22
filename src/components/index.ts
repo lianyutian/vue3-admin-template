660e9568b472800005209aa231bd4f391e2648d5
憶初陽的那缕光 <1436159527@qq.com>
2023-12-22 23:01:01 +0800

    build: src别名、环境变量、svg、sass、mock、axios集成

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,13 @@ import SvgIcon from './SvgIcon/index.vue'
 import type { App, Component } from 'vue'
 
 const components: { [name: string]: Component } = { SvgIcon }
-
+//对外暴露插件对象
 export default {
+  //务必叫做install方法
   install(app: App) {
+    //注册项目全部的全局组件
     Object.keys(components).forEach((key) => {
+      //注册为全局组件
       app.component(key, components[key])
     })
   },
