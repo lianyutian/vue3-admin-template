1b7ff3ce0f4c4ac53103953c8e3599a1ba051d6b
憶初陽的那缕光 <1436159527@qq.com>
2023-12-22 23:01:01 +0800

    build: src别名、环境变量、svg、sass、mock、axios集成

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
+    <!-- svg:图标外层容器节点,内部需要与use标签结合使用 -->
     <svg :style="{ width: width, height: height }">
+      <!-- xlink:href执行用哪一个图标,属性值务必#icon-图标名字 -->
+      <!-- use标签fill属性可以设置图标的颜色 -->
       <use :xlink:href="prefix + name" :fill="color"></use>
     </svg>
   </div>
 </template>
 
 <script setup lang="ts">
+//接受父组件传递过来的参数
 defineProps({
   //xlink:href属性值的前缀
   prefix: {
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
-
+//引入模板的全局的样式
+import '@/styles/index.scss'
+import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //@ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+//svg插件需要配置代码
 import 'virtual:svg-icons-register'
+//引入自定义插件对象:注册整个项目全局组件
 import gloablComponent from './components/index'
-
+//获取应用实例对象
 const app = createApp(App)
-
-app
-  .use(ElementPlus, {
-    locale: zhCn,
-  })
-  .use(gloablComponent)
-  .mount('#app')
+//安装element-plus插件
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+//安装自定义插件
+app.use(gloablComponent)
+//将应用挂载到挂载点上
+app.mount('#app')
